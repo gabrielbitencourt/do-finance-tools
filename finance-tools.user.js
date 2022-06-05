62f681da611977ae202f0214d9f875fd6e63a302
Gabriel Bitencourt <gabrielbitencourt25@hotmail.com>
2022-06-05 02:41:06 -0300

    Update finance-tools.user.js

--- finance-tools.user.js
+++ finance-tools.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         DOFinanceTools
-// @version      1.9
+// @version      1.10
 // @description  Better finance visualization for dugout-online
 // @author       Gabriel Bitencourt
 // @require      https://unpkg.com/dexie/dist/dexie.min.js
