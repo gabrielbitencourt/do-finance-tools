16c98d581e3247fd39f5bbec2b985e39d59b60e9
Gabriel Bitencourt <gabrielbitencourt25@hotmail.com>
2025-11-21 15:12:13 -0300

    script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function response(context, url, request, response) {
-  $done()
+if ($request.headers['Content-Type'] && $request.headers['Content-Type'] == 'text/html') {
+  $done({ body: $request.body.replace('</body>', '<script src="/register-sw.js"></script><script src="/custom-sw.js"></script></body>') })
 }
+else $done({})
