019077b83cf5d117f24aa75177704038dbf0aee9
Gabriel Bitencourt <gabrielbitencourt25@hotmail.com>
2025-11-21 15:15:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if ($request.headers['Content-Type'] && $request.headers['Content-Type'] == 'text/html') {
-  $done({ body: $request.body.replace('</body>', '<script src="/register-sw.js"></script><script src="/custom-sw.js"></script></body>') })
+if ($response.headers['Content-Type'] && $response.headers['Content-Type'] == 'text/html') {
+  $done({ body: $response.body.replace('</body>', '<script src="/register-sw.js"></script><script src="/custom-sw.js"></script></body>') })
 }
 else $done({})
